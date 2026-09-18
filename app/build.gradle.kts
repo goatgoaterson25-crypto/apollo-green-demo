93419aefb0c2880c3ebbd34590bd1b76f5d4b53d
goatgoaterson25-crypto <goatgoaterson25@gmail.com>
2026-09-18 14:04:01 -0500

    Apollo: Injected build config

---
+++ app/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    id("com.android.application") version "8.2.0"
+}
+
+android {
+    namespace = "com.apollo.apollo-green-demo"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.apollo.apollo-green-demo"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
